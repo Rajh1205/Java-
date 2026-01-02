c908a66194eab023ed5e3c86d1979aef3d483cdf
Rajh1205 <rajhgodhani266@gmail.com>
2026-01-02 19:13:55 +0530

    Commit

---
+++ 23-12(Intro of java)/Example1.java
@@ -0,0 +1,20 @@
+package com2312;
+
+public class Example1 
+{
+public static void main(String[]args)
+{
+	int a =10;
+	float b = 10.3F;
+	char c = 'T';
+	double d = 10.9087;
+	boolean e = true;
+	String f = "Raj";
+	
+	System.out.println("Integer vlaue is: "+a);
+	System.out.println("Float value is: "+b);
+	System.out.println("char value is: "+c);
+	System.out.println("double value is: "+d);
+	System.out.println("boolean value is: "+e);
+}
+}
